23e81d1858d0bfafb7419492de075c52d20afdf0
kchung1068 <kchung1068@stu.d214.org>
2021-03-12 15:08:00 -0600

    G

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Person p = new Person();
+        Student s = new Student();
+        Object[] j =  {5, "T", 6.7, true};
+
+    }
+}
