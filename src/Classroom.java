fee9402bfdc8a6a454693e02f3bd095e9bdd0c70
kchung1068 <kchung1068@stu.d214.org>
2021-03-12 15:21:25 -0600

    Gmn

---
+++ src/Classroom.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Classroom {
+    public Teacher teacher;
+    public int roomNumber;
+    public ArrayList<String> students;
+    public int[] seats;
+
+}
