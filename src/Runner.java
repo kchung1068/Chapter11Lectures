5969745b2bf427ce30a4705275e269d1815b6129
kchung1068 <kchung1068@stu.d214.org>
2021-04-05 15:23:29 -0500

    Hum

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+
 public class Runner {
     public static void main(String[] args) {
     FirstClassSeat s1 = new FirstClassSeat();
@@ -12,8 +14,69 @@ public class Runner {
 
         /// Overloading is when the child and parent's methods have the same name but could be different in arguments or order of arguments
         //Over writing is when the methods of the child and parent are exactly the same.
+        Fish f = new Fish();
+
+        Animal a = new Animal();
+
+        Animal m = new Fish(); // m accesses the Animal methods. It can't access the fish methods
+        System.out.println(m instanceof Fish);
+
+
+//        Dog d = (Dog)(m);
+//        d.Bark();
+
+        System.out.println(m);
+        System.out.println(a);
 
 
 
+
+        Object o = new String("Hello");
+        //Object type, not the reference type
+        //String toString, not the Object toString
+        //If the method isn't shared, then it goes by the reference
+        //If the method is shared, it goes by the Object
+
+        if (o instanceof String) {
+            ((String) o).length();
+        }
+
+        System.out.println(o);
+    }
+}
+class Fish extends Animal {
+    private String scaleColor;
+        public Fish() {
+
+        }
+    public Fish(String name) {
+
+    }
+
+    public void setScaleColor(String scaleColor) {
+        this.scaleColor = scaleColor;
+    }
+
+    public String getScaleColor() {
+        return scaleColor;
+    }
+    public void swim() {
+        System.out.println("Swimming");
+    }
+}
+class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+class Dog extends Animal {
+    public void Bark() {
+        System.out.println("Bark");
     }
 }
